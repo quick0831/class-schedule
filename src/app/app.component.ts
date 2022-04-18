6b6222027c78b48abd66c6a9a2313a811b2ed941
Quick Chang <quick0831@gmail.com>
2022-04-18 23:46:29 +0800

    complete data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,12 +19,21 @@ export class AppComponent {
     "pra1": ["介面電路實習", "https://meet.google.com/pjc-yfzw-bea"],
     "pra2": ["專題製作", "https://meet.google.com/xpe-qfno-spq"],
     "pra3": ["智慧電子應用實習", "https://meet.google.com/mgf-rudw-umo"],
-    "class": ["班會", "https://meet.google.com/bqe-caat-edk"],
+    "cl": ["班會", "https://meet.google.com/bqe-caat-edk"],
     "ele": ["基本電學", "https://meet.google.com/ncw-hxow-dpx"],
-    "pe": ["體育", ""]
+    "pe": ["體育", ""],
+    "geo": ["地理", ""]
   };
 
   // use null as continuous class
-  table = [];
+  table = [
+    ["pro2", "pro1", "math", "pro1", "ele" ],
+    ["pra1",  null , "pra3", "pro2",  null ],
+    [ null , "math",  null ,  null ,  null ],
+    [ null ,  null ,  null , "ch"  , "cl"  ],
+    ["pra2", "ch"  , "pe"  , "eng" , "geo" ],
+    [ null ,  null ,  null ,  null ,  null ],
+    [ null , "eng" , "eng" , "math", "ch"  ]
+  ];
 
 }
