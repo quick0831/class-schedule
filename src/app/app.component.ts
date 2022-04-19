f10babe4cd8d3572c78b1d7d7cd3beb07e7ee29f
Quick Chang <quick0831@gmail.com>
2022-04-19 11:17:47 +0800

    rework of class data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  class_1 = ["", ""];
-  class_2 = ["", ""];
-
   classes: { [key: string]: [string, string] } = {
     "ch": ["國文", "https://meet.google.com/wwy-bzuu-hcx"],
     "eng": ["英文", "https://meet.google.com/bcv-xrwj-qze"],
@@ -48,5 +45,56 @@ export class AppComponent {
       }
     }
     this.class_length = a;
+  }
+
+  get_class(c: string): [string, string] {
+    return this.classes[c];
+  }
+
+  get weekday(): number {
+    return new Date().getDay()-1;
+  }
+
+  get class_num(): number {
+    let i = ((): number => {
+      let d = new Date();
+      let t = d.getHours() * 60 + d.getMinutes();
+      if(t < 500) return -1;
+      if(t < 550) return 0;
+      if(t < 610) return 1;
+      if(t < 670) return 2;
+      if(t < 730) return 3;
+      //if(t < 790) return -1; // noon break
+      if(t < 840) return 4;
+      if(t < 900) return 5;
+      if(t < 960) return 6;
+      return -1;
+    })();
+    while(i>=0 && this.class_length[i][this.weekday]==-1)
+      i -= 1;
+    return i;
+  }
+
+  get next_class_num(): number {
+    let i = this.class_num + 1;
+    while(i<7 && this.class_length[i][this.weekday]==-1)
+      i += 1;
+    if(i == 7)
+      return -1;
+    return i;
+  }
+
+  get class_1(): [string, string] {
+    let i = this.class_num;
+    if(i < 0)
+      return ["", ""];
+    return this.get_class(this.table[i][this.weekday])
+  }
+
+  get class_2(): [string, string] {
+    let i = this.next_class_num;
+    if(i < 0)
+      return ["", ""];
+    return this.get_class(this.table[i][this.weekday])
   }
 }
--- src/app/time-table/time-table.component.ts
+++ src/app/time-table/time-table.component.ts
@@ -13,35 +13,14 @@ export class TimeTableComponent implements OnInit {
   @Input() classes: { [key: string]: [string, string] } = {};
   @Input() class_length: number[][] = [];
 
+  @Input() weekday = 0;
+  @Input() class_num = 0;
+
   ngOnInit(): void {
   }
 
   get_class(c: string): [string, string] {
     return this.classes[c];
-  }
-
-  get weekday(): number {
-    return new Date().getDay()-1;
-  }
-
-  get class_num(): number {
-    let i = ((): number => {
-      let d = new Date();
-      let t = d.getHours() * 60 + d.getMinutes();
-      if(t < 500) return -1;
-      if(t < 550) return 0;
-      if(t < 610) return 1;
-      if(t < 670) return 2;
-      if(t < 730) return 3;
-      //if(t < 790) return -1; // noon break
-      if(t < 840) return 4;
-      if(t < 900) return 5;
-      if(t < 960) return 6;
-      return -1;
-    })();
-    while(i>=0 && this.class_length[i][this.weekday]==-1)
-      i -= 1;
-    return i;
   }
 
 }
